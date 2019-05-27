31c142bc7ad7fb7f9b6ff64ea0c67b281aa78e45
Tobias Kolstrup <5884577@RTS-365.dk>
2019-05-27 10:26:05 +0200

    typeof if statement

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,13 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home', {"title": "Home"});
    });
 
    app.get('/product', (req, res, next) => {
-      res.render('product');
+      res.render('product', {"title": "Products"});
    });
 
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact', {"title": "Contact"});
    });
 };
