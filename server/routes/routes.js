b3afb7d09587c91896bb8521259136ac95a100c2
Tobias Kolstrup <5884577@RTS-365.dk>
2019-05-27 10:16:24 +0200

    Done phase 1

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,4 +2,12 @@ module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home');
    });
+
+   app.get('/product', (req, res, next) => {
+      res.render('product');
+   });
+
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
+   });
 };
