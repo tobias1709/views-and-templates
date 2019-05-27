8c7946c27e03ba73cf17e0956a5adcd7de9fc359
Tobias Kolstrup <5884577@RTS-365.dk>
2019-05-27 13:25:15 +0200

    test

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,9 +5,16 @@ module.exports = (app) => {
 
    app.get('/product', (req, res, next) => {
       res.render('product', {"title": "Products"});
+      // res.send();
+      // console.log("hello");
    });
 
    app.get('/contact', (req, res, next) => {
       res.render('contact', {"title": "Contact"});
+   });
+
+   app.get('/test', (req, res, next) => {
+      res.send();
+      console.log("Test");
    });
 };
